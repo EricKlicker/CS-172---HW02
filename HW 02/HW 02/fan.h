0af3f5b081da202420abc19aea2ed66d86824bcd
Eric Klicker <ericklicker@yahoo.com>
2016-09-26 14:56:08 -0700

    start of HW03

---
+++ HW 02/HW 02/fan.h
@@ -0,0 +1,31 @@
+//
+//  fan.h
+//  HW 02
+//
+//  Created by Eric Klicker on 9/26/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+#ifndef fan_h
+#define fan_h
+
+
+#endif 
+
+
+
+class fan
+{
+private:
+    
+    double radius;
+    
+public:
+    int speed;
+    bool on;
+    
+    
+    
+};
---
+++ HW 02/HW 02/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  HW 02
+//
+//  Created by Eric Klicker on 9/25/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+#include "fan.h"
+using namespace std;
+
+
+int main ()
+{
+    
+    fan f1,f2,f3;
+    f1.speed = 1;
+    f2.speed = 2;
+    f3.speed = 3;
+
+    //how to implement bool
+    //how to implement radius with user entering
+    //how to do no-arg constructor
+    
+    
+}
+
